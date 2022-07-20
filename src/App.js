294229be9fd2280226762bd2eee36fc97fa6b48d
Myles Jeffery <mylesjeffery96@gmail.com>
2022-07-19 18:25:14 -0700

    comcom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Typewriter from 'typewriter-effect'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Typewriter
+				options={{ delay: 110, cursor: '|' }}
+				onInit={(typewriter) => {
+					typewriter
+						.pauseFor(4000)
+						.typeString("hey, how's it going?")
+						.pauseFor(1000)
+						.typeString(' welcome to my website.')
+						.pauseFor(1000)
+						.typeString(
+							'<br>i\'m Myles, a user experience designer working at <a href="https://ammunitiongroup.com/" target="_blank">ammunition</a>.'
+						)
+						.pauseFor(1000)
+						.typeString(
+							'<br>feel free to <a href="mailto:mylesjeffery96@gmail.com">email me</a> or connect with me on <a href="https://www.linkedin.com/in/myles-jeffery/" target="_blank">linkedin</a>.'
+						)
+						.pauseFor(1000)
+						.typeString(
+							'<br>portfolio and résumé available upon request.'
+						)
+						.pauseFor(1000)
+						.typeString('<br>have a great day! 😎')
+						.start()
+				}}
+			/>
+		</div>
+	)
 }
 
-export default App;
+export default App
